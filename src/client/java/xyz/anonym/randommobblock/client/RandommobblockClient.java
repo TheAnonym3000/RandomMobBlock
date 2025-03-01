a511b1757c4edf45436afe8377ce210093a17d15
theanonym3000 <186270664+TheAnonym3000@users.noreply.github.com>
2025-03-01 08:42:13 +0100

    Made help annotations for R40Fendt (Fixed typing error) and reformatted.

--- src/client/java/xyz/anonym/randommobblock/client/RandommobblockClient.java
+++ src/client/java/xyz/anonym/randommobblock/client/RandommobblockClient.java
@@ -14,7 +14,9 @@ import java.util.Random;
 
 public class RandommobblockClient implements ClientModInitializer {
     public static final String MOD_ID = "randommobblock";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);@Override
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
     public void onInitializeClient() {
         LOGGER.info("Initializing...");
         List<String> mobs = Arrays.asList("ALLAY", "ARMADILLO", "AXOLOTL", "BAT", "BEE", "BLAZE", "BOGGED", "BREEZE", "CAMEL", "CAT", "CAVE_SPIDER", "CHICKEN", "COD", "COW", "CREAKING", "CREEPER", "DOLPHIN", "DONKEY", "DROWNED", "ENDERMAN", "ENDERMITE", "EVOKER", "FOX", "FROG", "GHAST", "GLOW_SQUID", "GOAT", "GUARDIAN", "HOGLIN", "HORSE", "HUSK", "ILLUSIONER", "IRON_GOLEM", "LLAMA", "MAGMA_CUBE", "MOOSHROOM", "MULE", "OCELOT", "PANDA", "PARROT", "PHANTOM", "PIG", "PIGLIN", "PIGLIN_BRUTE", "PILLAGER", "POLAR_BEAR", "PUFFERFISH", "RABBIT", "RAVAGER", "SALMON", "SHEEP", "SHULKER", "SILVERFISH", "SKELETON", "SKELETON_HORSE", "SLIME", "SNIFFER", "SNOW_GOLEM", "SPIDER", "SQUID", "STRAY", "STRIDER", "TADPOLE", "TRADER_LLAMA", "TROPICAL_FISH", "TURTLE", "VEX", "VILLAGER", "VINDICATOR", "WANDERING_TRADER", "WITCH", "WITHER_SKELETON", "WOLF", "ZOGLIN", "ZOMBIE", "ZOMBIE_HORSE", "ZOMBIE_VILLAGER", "ZOMBIFIED_PIGLIN");
@@ -29,7 +31,7 @@ public class RandommobblockClient implements ClientModInitializer {
                 //String temp = map.get(blockstate.getBlock().toString())
             //})
             try {
-                //noinspection OptionalGetWithoutIsPresent
+                //noinspection OptionalGetWithoutIsPresent (Try-Catch-STatement is  here to fix it!)
                 EntityType.get(temp).get().spawn((ServerWorld) world, blockPos, SpawnReason.EVENT);
             } catch (Exception e) {
                 LOGGER.warn("I made a serious mistake while coding...");
